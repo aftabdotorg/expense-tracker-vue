43a795f4ff65b0831f66b01cfe468950b5e9a775
aftabdotorg <aft4b.akhand@gmail.com>
2024-01-14 23:14:31 +0530

    minor change

--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -5,28 +5,20 @@
             {{ text }} <span>${{ amount }}</span>
             <button class="delete-btn" @click="deleteTransaction(id)">x</button>
         </li>
-        <!-- <li class="minus">
-          Cash <span>-$400</span><button class="delete-btn">x</button>
-        </li>
-        <li class="plus">
-          Paycheck <span>-$800</span><button class="delete-btn">x</button>
-        </li> -->
     </ul>
 </template>
 
 <script setup>
-
-const emit = defineEmits(['transactionDeleted'])
-
+const emit = defineEmits(['transactionDeleted']);
 const props = defineProps({
     transactions: {
         type: Array,
         required: true
     }
-})
+});
 
 const deleteTransaction = (id) => {
-    emit('transactionDeleted', id)
+    emit('transactionDeleted', id);
 }
 
 </script>
